12e1afbc66b27b24459cf3308172d0a685efa226
myftari09 <41291278+myftari09@users.noreply.github.com>
2020-10-29 11:43:47 +0100

    h

---
+++ assets/js/random.js
@@ -0,0 +1,14 @@
+  //Get a reference to the button
+  var button = document.getElementById("generate");
+
+  //Run the function on button click
+  button.onclick = function generate() {
+
+  // Create a variable to hold a random number between 1 and 100
+  var randomNum = Math.floor((Math.random() * 9000) + 1000);
+
+  // Create a variable el to hold the ranNum element
+  var el = document.getElementById('ranNum');
+  // Write the number into that element
+  el.innerHTML = randomNum;
+ }
